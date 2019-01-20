813269456b0a0526c30e9ae2850d3d05cc6dcf24
Mouhmmad Moujarkash <mouhmmad488@gmail.com>
2019-01-20 09:42:51 +0200

    created nav graph and added navigation controller and created the categories fragment and posts fragment

---
+++ app/src/main/java/com/project/mod/fawasell/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.project.mod.fawasell.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.project.mod.fawasell.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
